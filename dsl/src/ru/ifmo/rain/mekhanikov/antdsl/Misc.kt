c2f648e892f3eafc613dcead3abd0551b037a53a
Denis Mekhanikov <dmekhanikov@gmail.com>
2014-04-15 00:32:29 +0400

    Add code converted from update_dependencies.xml from Kotlin repository

---
+++ dsl/src/ru/ifmo/rain/mekhanikov/antdsl/Misc.kt
@@ -0,0 +1,7 @@
+package ru.ifmo.rain.mekhanikov.antdsl
+
+import java.util.regex.Pattern
+
+public fun matches(pattern: String, string: String): Boolean {
+    return Pattern.compile(pattern).matcher(string).matches()
+}
