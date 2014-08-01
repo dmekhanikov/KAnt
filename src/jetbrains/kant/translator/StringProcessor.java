fd310c347495589b78a0254995c7b92b81d326a7
Denis Mekhanikov <dmekhanikov@gmail.com>
2014-08-01 14:48:38 +0400

    Escape $$

--- src/jetbrains/kant/translator/StringProcessor.java
+++ src/jetbrains/kant/translator/StringProcessor.java
@@ -34,27 +34,36 @@ public class StringProcessor {
         }
     }
 
+    public static boolean isStartOfTemplate(String string, int i) {
+        return (string.charAt(i) == '$' || string.charAt(i) == '@') &&
+                (i == 0 || string.charAt(i - 1) != string.charAt(i)) &&
+                i + 1 < string.length() && string.charAt(i + 1) == '{';
+    }
+
     public static String processProperties(String value, PropertyManager propertyManager) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < value.length(); i++) {
-            if (i + 1 < value.length() &&
-                    (value.charAt(i) == '$' || value.charAt(i) == '@') &&
-                    value.charAt(i + 1) == '{') {
+            if (isStartOfTemplate(value, i)) {
                 boolean isAttribute = value.charAt(i) == '@';
                 StringBuilder propertyName = new StringBuilder();
                 for (i += 2; i < value.length() && value.charAt(i) != '}'; i++) {
                     propertyName.append(value.charAt(i));
                 }
-                if (propertyManager != null && !isAttribute) {
-                    propertyManager.readAccess(propertyName.toString());
-                }
-                String name = toCamelCase(propertyName.toString());
-                if (i + 1 < value.length() && Character.isJavaIdentifierPart(value.charAt(i + 1))) {
-                    name = "${" + name + "}";
+                String name = propertyName.toString();
+                if (name.equals("\"$\"")) {
+                    result.append("${\"$\"}");
                 } else {
-                    name = "$" + name;
+                    if (propertyManager != null && !isAttribute) {
+                        propertyManager.readAccess(propertyName.toString());
+                    }
+                    name = toCamelCase(name);
+                    if (i + 1 < value.length() && Character.isJavaIdentifierPart(value.charAt(i + 1))) {
+                        name = "${" + name + "}";
+                    } else {
+                        name = "$" + name;
+                    }
+                    result.append(name);
                 }
-                result.append(name);
             } else {
                 result.append(value.charAt(i));
             }
@@ -62,15 +71,25 @@ public class StringProcessor {
         return result.toString();
     }
 
-    public static String escapeTemplates(String value) {
+    private static boolean isIdentifierStart(char c) {
+        return Character.isLetter(c) || c == '_';
+    }
+
+    public static String escapeTemplates(String value) { //don't forget about @@ inside macrodefs
         StringBuilder result = new StringBuilder(value);
-        for (int i = result.indexOf("$"); i != -1; i = result.indexOf("$", i)) {
-            if (result.length() > i + 1 &&
-                    (Character.isLetter(result.charAt(i + 1)) || result.charAt(i + 1) == '_')) {
-                result.replace(i, i + 1, "${\"$\"}");
-                i += 6;
-            } else {
-                i++;
+        for (int i = 0; i < result.length() - 1; i++) {
+            if (result.charAt(i) == '$') {
+                if (isIdentifierStart(result.charAt(i + 1))) {
+                    result.insert(i + 1, "{\"$\"}");
+                    i += 5;
+                } else if (result.charAt(i + 1) == '$') {
+                    if (i + 2 < result.length() &&
+                            (isIdentifierStart(result.charAt(i + 2)) || result.charAt(i + 2) == '{')) {
+                        result.replace(i + 1, i + 2, "{\"$\"}");
+                    } else {
+                        result.deleteCharAt(i);
+                    }
+                }
             }
         }
         return result.toString();
